6e4491d843079806ed099df25d214f03df930fc5
ProfessionalIT <leandro@professionalit.com.br>
2015-05-19 16:31:16 -0300

    Adicionado novas models e o modulo de admin.

--- twd/twd/urls.py
+++ twd/twd/urls.py
@@ -4,10 +4,6 @@ from django.conf import settings
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'twd.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
